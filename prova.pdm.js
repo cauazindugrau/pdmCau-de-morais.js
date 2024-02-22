1e48c5fee3f07f68bfd2a6fd73ac30b180987554
cauazindugrau <125456239+cauazindugrau@users.noreply.github.com>
2024-02-22 12:36:51 -0300

    Add files via upload

---
+++ prova.pdm.js
@@ -0,0 +1,43 @@
+let aluno1 = {//cria a variavel
+nome: "isabellakrai de morais",
+idade: 19,
+cursando: {"informática" - "literatura" - "logística" - "história"},
+presença: { "13/10;2024" - "05/05/2024" - "05/04/2024" - "18/07/2024"}                                  }
+ra: "204088"
+}
+let aluno2 = {//segunda variavel
+    nome: "cauã Barroso",
+    idade: 13,
+    cursando: "biotecnologia" - "matemática" - "logística" - "física",
+    presença:  "13/10;2024" - "05/05/2024" - "05/04/2024" - "18/07/2024",
+    ra: "090425"
+}
+
+
+function CadastroAlunos() {
+
+if (aluno.nome <10){
+} else {
+    console.log("Nome possui menos que 10 caracteres")
+}
+if (aluno.idade <14){
+} else {
+    console.log("Idade menor que 14")
+}
+if (aluno.nome cursando<10){
+} else {
+console.log("Cursando menos que 3 matérias")if (aluno.nome <10){
+if (aluno.presença <10){
+} else {
+console.log("Possui menos que 2 presenças")
+}
+
+
+
+
+
+
+
+
+
+
